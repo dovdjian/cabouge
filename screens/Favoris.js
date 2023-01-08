27257295691391477db151e71b8e7613b7ad4629
dodjian <dovdjianpro@gmail.com>
2023-01-08 19:47:20 +0100

    [+] TODO: week calendar

--- screens/Favoris.js
+++ screens/Favoris.js
@@ -1,12 +1,9 @@
-import { Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { View } from "react-native";
 
 export default function Favoris({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-        <Text>Go back to Home</Text>
-      </TouchableOpacity>
-    </View>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    ></View>
   );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,70 +1,15 @@
-import { useState } from "react";
-import { Modal, Pressable, Text, View } from "react-native";
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
-import { StyleSheet } from "react-native-web";
+import { StyleSheet, View } from "react-native";
 
 export default function Home({ navigation }) {
-  const generatePeople = () => {
-    let peoples = [];
-    for (let i = 0; i < 30; i++) {
-      peoples.push({
-        name: `Person ${i + 1}`,
-        age: Math.floor(Math.random() * 100),
-      });
-    }
-    return peoples;
-  };
-  const [modalVisible, setModalVisible] = useState(false);
-  const [itemAge, setItemAge] = useState(0);
-  const handlePress = (item) => {
-    console.log(item.age);
-    setItemAge(item.age);
-    setModalVisible(true);
-  };
-  const handlePressFavoris = () => {
-    navigation.navigate("Favoris");
-    setModalVisible(true);
-  };
-  const [people] = useState(generatePeople());
-
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={!modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <TouchableOpacity onPress={() => handlePressFavoris()}>
-          <Text>Go to Favoris</Text>
-        </TouchableOpacity>
-        <FlatList
-          data={people}
-          renderItem={({ item }) => (
-            <View>
-              <Pressable onPress={() => handlePress(item)}>
-                <Text style={styles.item}>{item.name}</Text>
-              </Pressable>
-            </View>
-          )}
-        />
-      </Modal>
-      {modalVisible && (
-        <Pressable onPress={() => setModalVisible(false)}>
-          <Text style={styles.item}>{itemAge}</Text>
-        </Pressable>
-      )}
-    </View>
-  );
+  return <View></View>;
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: "pink",
-    padding: 30,
-    marginTop: 24,
-    marginHorizontal: 10,
+  weekCalendarWrapper: {
+    flex: 1,
+    alignItems: "center",
+  },
+  weekCalendar: {
+    width: document.body.clientWidth,
   },
 });
